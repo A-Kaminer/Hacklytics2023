1361a75fcc9f8b8ca3f3895b9739297ad63e4994
Andrew Kaminer <andrewkaminer@gmail.com>
2023-02-11 03:53:06 -0500

    Squashed some bugs

--- webApp/analysis.py
+++ webApp/analysis.py
@@ -6,17 +6,16 @@ import string
 
 
 class Analysis:
-    def sample_plot():
+    def sample_plot(is_null):
         sp500 = pd.read_csv('./data/SP500.csv')
 
         sp500['DATE'] = pd.to_datetime(sp500['DATE'], format='%Y-%m-%d')
         sp500['SP500'] = pd.to_numeric(sp500['SP500'], errors='coerce')
         sp500['SP500'] = sp500['SP500'].dropna()
 
-        plt.clf()
-        plt.plot(sp500['DATE'], sp500['SP500'])
+        fig = plt.Figure()
+        ax = fig.add_subplot(1,1,1)
+        if not is_null == "True":
+            ax.plot(sp500['DATE'], sp500['SP500'])
 
-        filename = './static/plot.png'
-        plt.savefig(filename)
-
-        return filename
+        return fig
--- webApp/app.py
+++ webApp/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, url_for, send_file, Response
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from analysis import Analysis
+import io
 
 app = Flask(__name__)
 
@@ -9,8 +11,15 @@ def index():
 
 @app.route('/plot')
 def plot():
-    filename = Analysis.sample_plot()
-    resp = Response(render_template('plot.html', img_name=filename))
+    resp = Response(render_template('plot.html'))
+    return resp
+
+@app.route('/plot.png/<is_null>')
+def plot_png(is_null):
+    fig = Analysis.sample_plot(is_null)
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    resp = Response(output.getvalue(), mimetype='image/png')
     resp.headers['Cache-Control'] = 'no-cache'
     return resp
 
